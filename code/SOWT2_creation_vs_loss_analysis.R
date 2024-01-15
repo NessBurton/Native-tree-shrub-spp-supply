1ffba6d3656cc297db12baf5fc16c37918d59191
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2024-01-15 16:44:03 +0000

    Clean data.

--- code/SOWT2_creation_vs_loss_analysis.R
+++ code/SOWT2_creation_vs_loss_analysis.R
@@ -14,14 +14,43 @@ dirOut <- paste0(wd,"data-out")
 
 library(tidyverse)
 library(ggplot2)
+library(stringr)
 
 ### read in data --------------------------------------------------------------------------------------
 
 # downloaded from https://www.forestresearch.gov.uk/tools-and-resources/statistics/data-downloads/
 # on 15-01-2024
 
-data_area <- read.csv(paste0(dirData,""))
+data_area <- read.csv(paste0(dirData,"area-timeseries-UK.csv"))
 
 ### have a look ---------------------------------------------------------------------------------------
 
+head(data_area)
+summary(data_area)
+colnames(data_area)
+colnames(data_area) <- c("year",
+                         "private.sector.cf",
+                         "public.sector.cf",
+                         "private.sector.bf",
+                         "public.sector.bf",
+                         "uk.total")
+
+# issue that a couple of variables are reading in as characters
+
+### clean ---------------------------------------------------------------------------------------------
+
+# strip out ","
+test <- str_replace_all(data_area$uk.total,",","")
+as.numeric(test)
+
+# all good, apply to data
+data_area$uk.total <- as.numeric(str_replace_all(data_area$uk.total,",",""))
+data_area$private.sector.bf <- as.numeric(str_replace_all(data_area$private.sector.bf,",",""))
+
+
+### plot it -------------------------------------------------------------------------------------------
+
+data_area %>% 
+  ggplot()+
+  geom_point(aes(x = year, y = uk.total))
 
