c2655ebfced2c29624c1f863431502778ffd3a8e
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2024-01-15 15:39:32 +0000

    Data paths.

--- code/SOWT2_creation_vs_loss_analysis.R
+++ code/SOWT2_creation_vs_loss_analysis.R
@@ -5,7 +5,22 @@
 
 ### working dirs --------------------------------------------------------------------------------------
 
+wd <- "~/Documents/Woodland-Trust/Data-Analysis/Project-SOWT2"
+dirData <- paste0(wd,"/data-raw/")
+dirScratch <- paste0(wd,"/data-scratch/")
+dirOut <- paste0(wd,"data-out")
+
+### libraries -----------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+
 ### read in data --------------------------------------------------------------------------------------
+
+# downloaded from https://www.forestresearch.gov.uk/tools-and-resources/statistics/data-downloads/
+# on 15-01-2024
+
+data_area <- read.csv(paste0(dirData,""))
 
 ### have a look ---------------------------------------------------------------------------------------
 
