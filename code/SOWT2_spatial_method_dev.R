c0afcd0d564dbbfa0d23ffbda20d4c5cf433e421
Vanessa Burton <vanessa.burton26@gmail.com>
2024-02-13 10:42:11 +0000

    Start script for spatial method development.

---
+++ code/SOWT2_spatial_method_dev.R
@@ -0,0 +1,18 @@
+
+# date: 13-02-24
+# author: VB
+# purpose: Explore spatial method for reproducing woodland communities
+
+### working dirs ---------------------------------------------------------------
+
+wd <- "C:/Users/vbu/OneDrive - the Woodland Trust/Projects/CO&E - SoWT2/Project-SoWT2" # WT laptop path
+dirData <- paste0(wd,"/data-raw/")
+dirScratch <- paste0(wd,"/data-scratch/")
+dirOut <- paste0(wd,"data-out")
+
+### libraries ------------------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(stars)
