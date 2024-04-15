0e6457f6dbc8e9a1ba4c8f96e444d019a94bfcb2
Vanessa Burton <vanessa.burton26@gmail.com>
2024-04-15 16:44:13 +0100

    Start processing digitised species maps.

--- code/SOWT2_spatial_method_dev.R
+++ code/SOWT2_spatial_method_dev.R
@@ -27,3 +27,31 @@ ggplot()+
   geom_sf(RoP_poly, mapping = aes(fill = as.factor(SEED_ZONES)))+
   labs(fill = "Native seed zones")+
   theme_minimal()
+
+### read in species shapefiles -------------------------------------------------
+
+lstSpecies <- c("Hornbeam", "Holly", "Hazel", "Guelder_rose")
+
+myPal <- c("4" = "#005A32",
+           "3" = "#41AB5D", 
+           "2" = "#1B9E77",
+           "1" = "darkkhaki",
+           "0" = "white")
+
+for (i in lstSpecies){
+  
+  #i <- lstSpecies[1]
+  
+  species <- st_read(paste0(dirData,i,".shp"))
+  
+  species$Code <- factor(species$Code, levels = c(NA,0,1,1.5,2,2.5,3,4))
+  
+  print(ggplot()+
+          geom_sf(species, mapping = aes(fill = Code))+
+          scale_fill_manual(values = myPal)+
+          labs(fill = "Appropriateness")+
+          ggtitle(i)+
+          theme_minimal())
+  
+}
+
