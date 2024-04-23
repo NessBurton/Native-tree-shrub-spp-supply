2281d389e4193a5f1c9999580acceab6428e6001
Vanessa Burton <vanessa.burton26@gmail.com>
2024-04-23 11:32:17 +0100

    Mostly cleaned and plotted. Some counties missing due to name mismatches remaining

--- code/SOWT2_spatial_method_dev.R
+++ code/SOWT2_spatial_method_dev.R
@@ -70,34 +70,59 @@ head(dfOpportunity)
 sfLA <- st_read(paste0(dirData,"Local_Authority_Districts_May_2022_UK_BFE_V3_2022_3331011932393166417/LAD_MAY_2022_UK_BFE_V3.shp"))
 head(sfLA)
 
-  unique(sfLA$LAD22CD)
-# filter to England
+# check codes - do these represent country?
+unique(sfLA$LAD22CD)
 
+# filter to England
 sfLA <- sfLA %>% filter(grepl("E",LAD22CD))
 
 # check whether LA names match
 dfOpportunity$local.authority %in% sfLA$LAD22NM
 unique(dfOpportunity$local.authority)
 unique(sfLA$LAD22NM)
-# need to strip "district" and "B" from most the the dfOpportunity entries
-words <- c("[()]", "London Boro")
+# need to strip "district" and "B" from most of the dfOpportunity entries
+words <- c("London Boro", "District B", "District")
 pat <-str_c(words, collapse = "|")
-words2 <- c("District B", " B")
-pat2 <- str_c(words2, collapse = "|")
-dfOpportunity$local.authority %>%  str_remove_all(pat) %>% str_remove_all(pat2) %>% trimws()
+(test <- dfOpportunity$local.authority %>% str_remove_all(pat) %>% trimws())
+gsub("\\(B\\)", "", test) %>% trimws("right")
 
-dfOpportunity$local.authority <- dfOpportunity$local.authority %>% str_remove_all(pat) %>% str_remove_all(pat2) %>% trimws()
+dfOpportunity$local.authority <- dfOpportunity$local.authority %>% str_remove_all(pat) %>% trimws() %>% gsub("\\(B\\)", "", .) %>% trimws("right")
 
+length(dfOpportunity$local.authority)
+length(sfLA$LAD22NM) # doesn't match
 dfOpportunity$local.authority %in% sfLA$LAD22NM #  some still don't match
+(issues <- dfOpportunity$local.authority[which(dfOpportunity$local.authority %in% sfLA$LAD22NM == FALSE)]) # these ones
+
+# city of causing some of the issues...
+words2 <- c("City and County of the ", "City of ", "The ", "County of ", "[.]")
+pat2 <-str_c(words2, collapse = "|")
+(test2 <- dfOpportunity$local.authority %>% str_remove_all(pat2) %>% trimws())
+dfOpportunity$local.authority <- dfOpportunity$local.authority %>% str_remove_all(pat2) %>% trimws()
+
+# do the same for sfLA
+(issues <- sfLA$LAD22NM[which(sfLA$LAD22NM %in% dfOpportunity$local.authority == FALSE)]) # these ones
+words3 <- c(", City of", ", County of")
+pat3 <-str_c(words3, collapse = "|")
+(test3 <- sfLA$LAD22NM %>% str_remove_all(pat3) %>% trimws())
+sfLA$LAD22NM <- sfLA$LAD22NM %>% str_remove_all(pat3) %>% trimws()
+
+# check again
+(issues <- dfOpportunity$local.authority[which(dfOpportunity$local.authority %in% sfLA$LAD22NM == FALSE)]) # these ones
+# erg. close enough for now
 
 # join
+sfLA <- sfLA %>% mutate(LA = LAD22NM)
+dfOpportunity <- dfOpportunity %>% mutate(LA = local.authority)
+
+sfOpportunity <- merge(sfLA, dfOpportunity, by = "LA")
 
 # plot LAs
 ggplot()+
-  geom_sf(sfLA, mapping = aes(fill = LAD22NM))+
+  geom_sf(sfOpportunity, mapping = aes(fill = woodland.opportunity.ha))+
   #scale_fill_brewer(type = "seq", palette = 2)+
   #labs(fill = "Appropriateness")+
   theme_minimal()
+
 
 ### read in regions of provenance ----------------------------------------------
 
