020cefbbe35f20d9524fdfc61e057f0b4eaea9ae
Vanessa Burton <vanessa.burton26@gmail.com>
2024-07-08 14:02:39 +0100

    Produce figure as jpg and svg

--- code/SOWT2_just_creation_stats.R
+++ code/SOWT2_just_creation_stats.R
@@ -8,7 +8,8 @@
 wd <- "C:/Users/vbu/OneDrive - the Woodland Trust/Projects/CO&E - SoWT2/Project-SoWT2" # WT laptop path
 dirData <- paste0(wd,"/data-raw/")
 dirScratch <- paste0(wd,"/data-scratch/")
-dirOut <- paste0(wd,"data-out")
+dirOut <- paste0(wd,"/data-out/")
+dirFigs <- paste0(wd,"/figures/")
 
 ### libraries ------------------------------------------------------------------
 
@@ -86,7 +87,7 @@ dfCreation_summary <- dfCreation_long %>%
   group_by(Year, Country) %>% 
   summarise(thousand.ha = sum(thousand.ha)) %>% 
   mutate(ha_created = thousand.ha*1000) %>% 
-  filter(Year >=2016 & Year <= 2024) %>% 
+  filter(Year >=2020 & Year <= 2024) %>% 
   group_by(Country) %>% 
   summarise(Achieved = mean(ha_created),
             SD = sd(ha_created)) %>% 
@@ -103,19 +104,19 @@ dfCreation_summary <- dfCreation_long %>%
 
 
 (p1 <- dfCreation_summary %>% 
-  gather(., Progress, Created_ha, c("Achieved", "Deficit"), factor_key = T) %>% 
-  ggplot()+
-  geom_col(aes(x = Country, y = Created_ha, fill = Progress), position = position_stack(reverse = TRUE))+
-  guides(fill = guide_legend(reverse = TRUE))+
-  #geom_errorbar(aes(x=Country, ymin = c(actual.av - actual.sd, rep(NA, length(actual.av))), ymax = c(actual.av + actual.sd, rep(NA, length(actual.av)))), width=.2) +
-  scale_fill_viridis(discrete = T, option = "D") +
-  scale_y_continuous(name = "Planting (ha/year)", limits = c(0, 38000), labels = scales::comma)+
-  scale_x_discrete(name = "Region")+
-  geom_text(aes(label = paste0(Percentage,"%"), y=CCC, x=Country, vjust= -0.5))+
-  ggtitle("Average annual planting 2016-2024\ncompared to CCC reccomendations") +
-  #theme_cowplot(12) +
-  theme_pubr()+
-  theme(text = element_text(family = "sans", color = "#22211d"),
+    gather(., Progress, Created_ha, c("Achieved", "Deficit"), factor_key = T) %>% 
+    mutate(Country = fct_relevel(Country, "England", "Northern Ireland", "Scotland", "Wales", "UK")) %>% 
+    ggplot()+
+    geom_col(aes(x = Country, y = Created_ha, fill = Progress), colour = "black", position = position_stack(reverse = TRUE))+
+    guides(fill = guide_legend(reverse = TRUE))+
+    scale_fill_grey(start = 0.9, end = 0.2)+
+    #geom_errorbar(aes(x=Country, ymin = c(actual.av - actual.sd, rep(NA, length(actual.av))), ymax = c(actual.av + actual.sd, rep(NA, length(actual.av)))), width=.2) +
+    scale_y_continuous(name = "Planting (ha/year)", limits = c(0, 38000), labels = scales::comma)+
+    scale_x_discrete(name = "Region")+
+    geom_text(aes(label = paste0(Percentage,"%"), y=CCC, x=Country, vjust= -0.5))+
+    ggtitle("Average annual planting 2020-2024\ncompared to CCC reccomendations") +
+    theme_bw()+
+    theme(text = element_text(family = "sans", color = "#22211d"),
         plot.title = element_blank(),
         plot.subtitle = element_text(size = 12),
         legend.title = element_blank(),
@@ -125,3 +126,7 @@ dfCreation_summary <- dfCreation_long %>%
         axis.title.y =  element_text(size = 12, vjust = 2.2 ),
         axis.title.x =  element_blank(),
         legend.position = "right"))
+
+ggsave(plot = p1 , paste0(dirFigs,"creation_rates.jpg"), width = 6, height = 3)
+library(svglite)
+ggsave(plot = p1 , paste0(dirFigs,"creation_rates.svg"), width = 6, height = 3)
